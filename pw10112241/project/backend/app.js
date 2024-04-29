7ac9465ba7d2b3d93dd0dcac7caedb80fbc42dfa
Karen Beltran <80376553+lkarenbeltran@users.noreply.github.com>
2024-04-29 10:58:48 -0700

    selecionar cliente

--- pw10112241/project/backend/app.js
+++ pw10112241/project/backend/app.js
@@ -1,47 +1,56 @@
-let express = require ('express');
+let express = require('express');
 let mysql = require('mysql');
-let cors = require ('cors');
+let cors = require('cors');
 
 let app = express();
 app.use(express.json());
 app.use(cors());
 
 //coneccion a MySQL
-    let conexion = mysql.createConnection({
-        host:'localhost',
-        user: 'root',
-        password:'',
-        database: 'pwdata',
-        port: '3306'
-    });
+let conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pwdata',
+    port: '3306'
+});
 
 //nos conectamos a  MySQL
-conexion.connect(function(error){
-if(error){
-    throw error;
-}
-console.log("conectado a la base de datos");
+conexion.connect(function (error) {
+    if (error) {
+        throw error;
+    }
+    console.log("conectado a la base de datos");
 });
 
 //rutas de acceso
-app.get ("/", function (req, res){
-res.send("ruta de inicio");
+app.get("/", function (req, res) {
+    res.send("ruta de inicio");
 });
 
-//Seleccionamos todos los clients
-app.get('/api/clientes', (req,res) =>{ 
-    conexion.query('SELECT * FROM clientes',(error, tuplas)=>{
-    if(error){
-        throw error;
-    } else{
-        res.send(tuplas);
-    }
-})
-});
+//Seleccionamon un cliente en especifico
+app.get('/api/cleintes/:id'), (req, res) => {
+    conexion.query('SELECT * FROM clientes WHERE id=?', [req.params.id], (error, tupla) => {
+        if (error) {
+            throw error;
+        } else {
+            res.send(tupla);
+        }
+    })
+};
+    //Seleccionamos todos los clients
+    app.get('/api/clientes', (req, res) => {
+        conexion.query('SELECT * FROM clientes', (error, tuplas) => {
+            if (error) {
+                throw error;
+            } else {
+                res.send(tuplas);
+            }
+        })
+    });
+        //encender servidor
+        let puerto = 3000;
+        app.listen(puerto, function () {
+            console.log("servidor escuchando puerto " + puerto);
 
-//encender servidor
-let puerto = 3000;
-app.listen(puerto, function(){
-    console.log("servidor escuchando puerto " + puerto);
-
-});
+        });
